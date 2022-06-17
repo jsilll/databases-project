ca46f474ef5fe4b5a409d10de32358192599a50a
joao <joao.freixial.silveira@tecnico.ulisboa.pt>
2022-06-17 17:17:54 +0100

    organized folder structure

---
+++ web/connection.py
@@ -0,0 +1,14 @@
+from os import environ
+from psycopg2 import connect
+from dotenv import load_dotenv
+
+load_dotenv()
+
+HOST = environ['DB_HOST']
+DATABASE = environ['DB_NAME']
+USER = environ['DB_USERNAME']
+PASSWORD = environ['DB_PASSWORD']
+
+def get_db_connection():
+    conn = connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD)
+    return conn
