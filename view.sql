113324763ac30f0998349025b16a706749653771
ines <=>
2022-06-19 15:34:49 +0100

    view done, olap not sure yet

---
+++ view.sql
@@ -0,0 +1,9 @@
+CREATE VIEW sales(ean, category_name, sale_year, quarter, day_of_month, day_of_week, district, county, nr_units)
+AS
+SELECT ean, category_name,
+EXTRACT(YEAR FROM instant) AS sale_year,
+EXTRACT(QUARTER FROM instant) AS quarter,
+EXTRACT(DAY FROM instant) AS day_of_month,
+EXTRACT(DOW FROM instant) AS day_of_week,
+district, county, nr_units
+FROM replenishment_event NATURAL JOIN shelve NATURAL JOIN installed_at NATURAL JOIN point_of_retail;
