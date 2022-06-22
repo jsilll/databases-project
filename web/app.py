961d09584a433723c46901de9492aaaeab1ea359
joao <joao.freixial.silveira@tecnico.ulisboa.pt>
2022-06-22 14:12:33 +0100

    finished app

--- web/app.py
+++ web/app.py
@@ -1,24 +1,16 @@
+from urllib import response
 from flask import Flask
-from flask import (render_template, 
-                   request, 
-                   url_for, 
-                   redirect, 
+from flask import (render_template,
+                   request,
+                   url_for,
+                   redirect,
                    abort)
-
-from psycopg2 import (InterfaceError,
-                      DatabaseError,
-                      DataError,
-                      OperationalError,
-                      IntegrityError,
-                      InternalError,
-                      ProgrammingError,
-                      NotSupportedError)
 
 from connection import get_db_connection
 
-# [ ] TODO: (perguntar) Inserir e remover um retalhista, com todos os seus produtos, garantindo que esta operação seja atómica;
+# [ ] Inserir e remover um retalhista, com todos os seus produtos, garantindo que esta operação seja atómica;
 # [x] Listar todos os eventos de reposição de uma IVM, apresentando o número de unidades repostas por categoria de produto
-# [ ] Inserir e remover categorias e sub-categorias
+# [x] Inserir e remover categorias e sub-categorias
 # [ ] Listar todas as sub-categorias de uma super-categoria, a todos os níveis de profundidade.
 
 # TODO: 3: parse date time?
@@ -29,17 +21,20 @@ app = Flask(__name__)
 
 # -------------- Home Page -------------- #
 
+
 @app.route("/", methods=["GET"])
 def index_page():
     return render_template("index.html")
 
 # -------------- Replenishments Page -------------- #
 
+
 @app.route("/replenishments/", methods=["GET"])
 def replenishments_page():
     return render_template("replenishments/replenishments.html")
 
 # --- List --- #
+
 
 @app.route("/replenishments/", methods=["POST"])
 def list_replenishments():
@@ -58,77 +53,151 @@ def list_replenishments():
                     'GROUP BY category_name ',
                     (serial_nr, manuf))
         res = cur.fetchall()
-    except (InterfaceError, OperationalError, InternalError, ProgrammingError, NotSupportedError):
-        abort(500)
-    except (DataError, IntegrityError):
-        abort(400)
-    except DatabaseError:
-        abort(500)
-    except Exception:
+    except Exception as e:
         abort(400)
     finally:
         cur.close()
         conn.close()
-    
+
     return render_template("replenishments/replenishments.html", serial_nr=serial_nr, manuf=manuf, table=res)
 
 # -------------- Categories Page -------------- #
+
 
 @app.route('/categories/', methods=["GET"])
 def categories_page():
     return render_template("categories/categories.html")
 
-# --- Create --- #
+# --- List --- #
+
 
 @app.route('/categories/', methods=["POST"])
 def list_categories():
-    # TODO: 
+    # TODO:
     print(request.form)
     return render_template("categories/categories.html")
 
 # --- Create --- #
 
+
 @app.route('/categories/create', methods=["GET"])
 def create_categories_page():
     return render_template("categories/create_category.html")
 
+
 @app.route('/categories/create/simple_category', methods=['POST'])
 def create_simple_category():
-    # TODO:
-    print(request.form)
+    name = request.form["name"]
+
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('INSERT INTO simple_category VALUES (%s)', (name))
+        conn.commit()
+    except Exception as e:
+        return str(e)
+    finally:
+        cur.close()
+        conn.close()
+
     return redirect(url_for('categories_page'))
+
 
 @app.route('/categories/create/super_category', methods=['POST'])
 def create_super_category():
-    # TODO:
-    print(request.form)
+    name = request.form["name"]
+    sub_category_name = request.form["sub_category_name"]
+
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('INSERT INTO super_category VALUES (%s)', (name))
+        cur.execute('INSERT INTO has_other VALUES (%s)',
+                    (name, sub_category_name))
+        conn.commit()
+    except Exception as e:
+        return str(e)
+    finally:
+        cur.close()
+        conn.close()
+
     return redirect(url_for('categories_page'))
+
 
 @app.route('/categories/create/sub_category', methods=['POST'])
 def create_sub_category():
-    # TODO:
-    print(request.form)
+    super_category_name = request.form["super_category_name"]
+    sub_category_name = request.form["sub_category_name"]
+
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('INSERT INTO has_other VALUES (%s)',
+                    (super_category_name, sub_category_name))
+        conn.commit()
+    except Exception as e:
+        return str(e)
+    finally:
+        cur.close()
+        conn.close()
+
     return redirect(url_for('categories_page'))
 
 # --- Delete --- #
+
 
 @app.route('/categories/delete/', methods=['GET'])
 def delete_categories_page():
     return render_template("categories/delete_category.html")
 
+
 @app.route('/categories/delete/simple_category', methods=['POST'])
 def delete_simple_category():
-    # TODO:
-    print(request.form)
+    name = request.form["name"]
+
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('DELETE FROM simple_category WHERE category_name = %s',
+                    (name))
+        cur.execute('DELETE FROM has_other WHERE category = %s',
+                    (name))
+        cur.execute('DELETE FROM has_category WHERE category_name = %s',
+                    (name))
+        conn.commit()
+    except Exception as e:
+        return str(e)
+    finally:
+        cur.close()
+        conn.close()
+
     return redirect(url_for('categories_page'))
+
 
 @app.route('/categories/delete/super_category', methods=['POST'])
 def delete_super_category():
-    # TODO:
-    print(request.form)
+    name = request.form["name"]
+
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('DELETE FROM super_category WHERE category_name = %s',
+                    (name))
+        cur.execute('DELETE FROM has_other WHERE super_category = %s',
+                    (name))
+        cur.execute('DELETE FROM has_category WHERE category_name = %s',
+                    (name))
+        conn.commit()
+    except Exception as e:
+        return str(e)
+    finally:
+        cur.close()
+        conn.close()
+
     return redirect(url_for('categories_page'))
 
 # -------------- Replenishments Page -------------- #
+
 
 @app.route("/retailers/", methods=["GET"])
 def retailers_page():
@@ -136,35 +205,73 @@ def retailers_page():
 
 # --- Create --- #
 
+
 @app.route("/retailers/create", methods=["GET"])
 def create_retailers_page():
     return render_template("retailers/create_retailer.html")
 
+
 @app.route("/retailers/create", methods=["POST"])
 def create_retailer():
-    # TODO:
-    print(request.form)
+    name = request.form["name"]
+    tin = request.form["tin"]
+    category_name = request.form["category_name"]
+    serial_nr = request.form["serial_nr"]
+    manuf = request.form["manuf"]
+
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('INSERT INTO retailer VALUES (%s, %s)',
+                    (tin, name))
+        cur.execute('INSERT INTO responsible_for VALUES (%s, %s, %s, %s, %s)',
+                    (category_name, tin, serial_nr, manuf))
+        conn.commit()
+    except Exception as e:
+        return str(e)
+    finally:
+        cur.close()
+        conn.close()
+
     return redirect(url_for('retailers_page'))
 
 # --- Delete --- #
 
+
 @app.route("/retailers/delete", methods=["GET"])
 def delete_retailers_page():
-    # TODO:
-    print(request.form)
     return render_template("retailers/delete_retailer.html")
+
 
 @app.route("/retailers/delete", methods=["POST"])
 def delete_retailer():
-    # TODO: 
-    print(request.form)
+    tin = request.form["tin"]
+
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('DELETE FROM retailer WHERE tin = %s',
+                    (tin))
+        cur.execute('DELETE FROM responsible_for WHERE tin = %s',
+                    (tin))
+        cur.execute('DELETE FROM replenishment_event WHERE tin = %s',
+                    (tin))
+        conn.commit()
+    except Exception as e:
+        return str(e)
+    finally:
+        cur.close()
+        conn.close()
+
     return redirect(url_for('retailers_page'))
 
 # -------------- Error Handlers -------------- #
 
+
 @app.errorhandler(404)
 def not_found_page():
     return render_template("errors/404.html"), 404
+
 
 @app.errorhandler(500)
 def internal_error_page():
--- web/connection.py
+++ web/connection.py
@@ -1,6 +1,16 @@
 from os import environ
 from psycopg2 import connect
 from dotenv import load_dotenv
+from functools import wraps
+from flask import abort
+from psycopg2 import (InterfaceError,
+                      DatabaseError,
+                      DataError,
+                      OperationalError,
+                      IntegrityError,
+                      InternalError,
+                      ProgrammingError,
+                      NotSupportedError)
 
 load_dotenv()
 
@@ -13,3 +23,28 @@ PASSWORD = environ["DB_PASSWORD"]
 def get_db_connection():
     conn = connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD)
     return conn
+
+
+def handle_transaction(transaction):
+    @wraps(transaction)
+    def decorated(*args, **kwargs):
+        g = transaction.__globals__
+        sentinel = object()
+        oldvalue = g.get('var', sentinel)
+
+        g['var'] = value
+        try:
+            return transaction()
+        except (InterfaceError, OperationalError, InternalError, ProgrammingError, NotSupportedError):
+            abort(500)
+        except (DataError, IntegrityError):
+            abort(400)
+        except DatabaseError:
+            abort(500)
+        except Exception:
+            abort(400)
+        finally:
+            cur.close()
+            conn.close()
+
+    return decorated
