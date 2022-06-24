a27a2f32fc0ff4c3f0b685c15d96b1adaf9ca809
João Silveira <joao.freixial.silveira@tecnico.ulisboa.pt>
2022-06-24 18:25:08 +0100

    prepared for deploying

--- web/connection.py
+++ web/connection.py
@@ -1,15 +1,19 @@
-from os import environ
+# from os import environ
 from psycopg2 import connect
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
-HOST = environ["DB_HOST"]
-DATABASE = environ["DB_NAME"]
-USER = environ["DB_USERNAME"]
-PASSWORD = environ["DB_PASSWORD"]
+# HOST = environ["DB_HOST"]
+# DATABASE = environ["DB_NAME"]
+# USER = environ["DB_USERNAME"]
+# PASSWORD = environ["DB_PASSWORD"]
 
+DB_HOST="db.tecnico.ulisboa.pt"
+DB_USER="istxxxxxxx"
+DB_DATABASE=DB_USER
+DB_PASSWORD="xxxxxxx"
 
 def get_db_connection():
-    conn = connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD)
+    conn = connect(host=DB_HOST, database=DB_DATABASE, user=DB_USER, password=DB_PASSWORD)
     return conn
